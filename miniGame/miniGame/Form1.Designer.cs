029d06d1227b0b3a06fdec92aca2b9350619fc5a
Kanezane <sgroi.andrea89@gmail.com>
2016-12-25 21:09:01 +0100

    test nome

--- miniGame/miniGame/Form1.Designer.cs
+++ miniGame/miniGame/Form1.Designer.cs
@@ -45,7 +45,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(360, 276);
+            this.label2.Location = new System.Drawing.Point(384, 98);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(72, 13);
             this.label2.TabIndex = 1;
