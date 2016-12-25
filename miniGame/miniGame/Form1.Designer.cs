4f41afa18ed141da3666d40e1f08c2e540d42058
zexirio <polocchia@gmail.com>
2016-12-25 21:03:05 +0100

    lel_1

--- miniGame/miniGame/Form1.Designer.cs
+++ miniGame/miniGame/Form1.Designer.cs
@@ -45,11 +45,11 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(202, 105);
+            this.label2.Location = new System.Drawing.Point(360, 276);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(177, 13);
+            this.label2.Size = new System.Drawing.Size(72, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "MI SCAPPA LA MERDA, SIGNORE";
+            this.label2.Text = "label spostata";
             // 
             // Form1
             // 
