06c5a5e0c6017ec7cbbdd29d5913a3663c9290cc
Kanezane <sgroi.andrea89@gmail.com>
2016-12-26 20:37:19 +0100

    Aggiunte di iCH

--- miniGame/miniGame/Form1.Designer.cs
+++ miniGame/miniGame/Form1.Designer.cs
@@ -28,28 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.serverIP = new System.Windows.Forms.TextBox();
+            this.hostingIP = new System.Windows.Forms.TextBox();
+            this.serverPORT = new System.Windows.Forms.TextBox();
+            this.hostingPORT = new System.Windows.Forms.TextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(491, 275);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(15, 15);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(29, 39);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 21);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "MI CONNEGGIO";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(29, 34);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "APRO IL SERVER";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(242, 178);
+            this.label1.Location = new System.Drawing.Point(272, 308);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(335, 81);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "miniGame";
+            this.label1.Size = new System.Drawing.Size(158, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "AHHHHHHHHHHHHHHHHHH";
             // 
-            // label2
+            // serverIP
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(78, 292);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(86, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "AHHHHHHHHH";
+            this.serverIP.Location = new System.Drawing.Point(193, 39);
+            this.serverIP.Name = "serverIP";
+            this.serverIP.Size = new System.Drawing.Size(146, 20);
+            this.serverIP.TabIndex = 4;
+            this.serverIP.Text = "25.108.14.68";
+            this.serverIP.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // hostingIP
+            // 
+            this.hostingIP.Location = new System.Drawing.Point(193, 34);
+            this.hostingIP.Name = "hostingIP";
+            this.hostingIP.Size = new System.Drawing.Size(146, 20);
+            this.hostingIP.TabIndex = 6;
+            this.hostingIP.Text = "25.108.14.68";
+            this.hostingIP.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // serverPORT
+            // 
+            this.serverPORT.Location = new System.Drawing.Point(345, 39);
+            this.serverPORT.Name = "serverPORT";
+            this.serverPORT.Size = new System.Drawing.Size(60, 20);
+            this.serverPORT.TabIndex = 5;
+            this.serverPORT.Text = "8888";
+            this.serverPORT.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // hostingPORT
+            // 
+            this.hostingPORT.Location = new System.Drawing.Point(345, 34);
+            this.hostingPORT.Name = "hostingPORT";
+            this.hostingPORT.Size = new System.Drawing.Size(60, 20);
+            this.hostingPORT.TabIndex = 7;
+            this.hostingPORT.Text = "8888";
+            this.hostingPORT.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(161, 65);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.button2);
+            this.splitContainer1.Panel1.Controls.Add(this.serverIP);
+            this.splitContainer1.Panel1.Controls.Add(this.serverPORT);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.button3);
+            this.splitContainer1.Panel2.Controls.Add(this.hostingIP);
+            this.splitContainer1.Panel2.Controls.Add(this.hostingPORT);
+            this.splitContainer1.Size = new System.Drawing.Size(444, 186);
+            this.splitContainer1.SplitterDistance = 94;
+            this.splitContainer1.TabIndex = 8;
             // 
             // Form1
             // 
@@ -57,11 +145,19 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(819, 437);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "miniGame";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.mover);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -69,8 +165,15 @@
 
         #endregion
 
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox serverIP;
+        private System.Windows.Forms.TextBox hostingIP;
+        private System.Windows.Forms.TextBox serverPORT;
+        private System.Windows.Forms.TextBox hostingPORT;
+        private System.Windows.Forms.SplitContainer splitContainer1;
     }
 }
 
--- miniGame/miniGame/Form1.cs
+++ miniGame/miniGame/Form1.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -17,9 +19,97 @@ namespace miniGame
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
+        TcpClient clientSocket = new TcpClient();
 
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+ 
+        }
+
+        public void msg(string mesg)
+        {
+            //textBox1.Text = textBox1.Text + Environment.NewLine + " >> " + mesg;
+        }
+
+        private void mover(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    Console.WriteLine("w");
+                    button1.Top -= 5;
+                    break;
+
+                case Keys.S:
+                    Console.WriteLine("s");
+                    button1.Top += 5;
+                    break;
+
+                case Keys.A:
+                    Console.WriteLine("a");
+                    button1.Left -= 5;
+                    break;
+
+                case Keys.D:
+                    Console.WriteLine("d");
+                    button1.Left += 5;
+                    break;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string textToSend = DateTime.Now.ToString();
+
+            //---create a TCPClient object at the IP and port no.---
+            TcpClient client = new TcpClient(serverIP.Text, Convert.ToInt32(serverPORT.Text));
+            NetworkStream nwStream = client.GetStream();
+            byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes(textToSend);
+
+            //---send the text---
+            Console.WriteLine("Sending : " + textToSend);
+            nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+
+            //---read back the text---
+            byte[] bytesToRead = new byte[client.ReceiveBufferSize];
+            int bytesRead = nwStream.Read(bytesToRead, 0, client.ReceiveBufferSize);
+            Console.WriteLine("Received : " + Encoding.ASCII.GetString(bytesToRead, 0, bytesRead));
+            Console.ReadLine();
+            if(client.Connected) {
+                MessageBox.Show("CRISTIDDIO E MIT LA PRIMAAAA");
+            }
+            client.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //---listen at the specified IP and port no.---
+            IPAddress localAdd = IPAddress.Parse(hostingIP.Text);
+            TcpListener listener = new TcpListener(localAdd, Convert.ToInt32(hostingPORT.Text));
+            Console.WriteLine("Listening...");
+            listener.Start();
+
+            //---incoming client connected---
+            TcpClient client = listener.AcceptTcpClient();
+            if (client.Connected) MessageBox.Show("Va");
+;            //---get the incoming data through a network stream---
+            NetworkStream nwStream = client.GetStream();
+            byte[] buffer = new byte[client.ReceiveBufferSize];
+
+            //---read incoming stream---
+            int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize);
+
+            //---convert the data received into a string---
+            string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            Console.WriteLine("Received : " + dataReceived);
+
+            //---write back the text to the client---
+            Console.WriteLine("Sending back : " + dataReceived);
+            nwStream.Write(buffer, 0, bytesRead);
+            client.Close();
+            listener.Stop();
+            //Console.ReadLine();
         }
     }
 }
